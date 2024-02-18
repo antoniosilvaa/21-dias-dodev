c8be10d9f2e80664046c60becd9aece6ef45334c
antonio silva <antg3102@gmail.com>
2024-02-18 15:38:20 -0300

    (exercicios do dia 03)

---
+++ dia 03/index.js
@@ -0,0 +1,19 @@
+let nome = ""
+let idade = 0
+let peso = 0
+let altura = 0
+
+nome = prompt("digite seu nome:" )
+idade = parseInt(prompt("digite sua idade:" ))
+peso = parseInt(prompt("digite seu peso:" ))
+altura = parseFloat(prompt("digite sua altura:" ))
+
+let anoNasc = 0 
+anoNasc = 2024 - idade 
+
+let imc = 0
+imc = peso / (altura * altura)
+
+console.log ("Olá," + nome, "Você tem," + idade, "anos,", "nasceu em," + anoNasc,
+"tem," + altura, "pesa," + peso, "seu imc è," + imc)
+
