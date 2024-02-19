59e6edde3bd05117214b096245e48fa55091ca72
antonio silva <antg3102@gmail.com>
2024-02-18 23:44:21 -0300

    exercicio do dia 03

--- dia 03/index.js
+++ dia 03/index.js
@@ -3,10 +3,10 @@ let idade = 0
 let peso = 0
 let altura = 0
 
-nome = prompt("digite seu nome:" )
-idade = parseInt(prompt("digite sua idade:" ))
-peso = parseInt(prompt("digite seu peso:" ))
-altura = parseFloat(prompt("digite sua altura:" ))
+nome = prompt("digite seu nome :" )
+idade = parseInt(prompt("digite sua idade :" ))
+peso = parseInt(prompt("digite seu peso :" ))
+altura = parseFloat(prompt("digite sua altura :" ))
 
 let anoNasc = 0 
 anoNasc = 2024 - idade 
