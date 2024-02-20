48026ef840cacfee502e1104fbd4770ee1848751
antonio silva <antg3102@gmail.com>
2024-02-20 14:00:22 -0300

    projetos dia 06

---
+++ dia 06/index.js
@@ -0,0 +1,11 @@
+let numero = parseInt(prompt("diga um numero inteiro e um numero positivo"))
+
+for (let i = 0; i <= numero; i++){
+console.log(i)
+}
+for (let i = 0; i <= 50; i += 5){
+    console.log(i)
+}
+for (let i = 50; i >= 0; i -= 5){
+    console.log(i)
+}
---
+++ dia 06/projeto 2/index.js
@@ -0,0 +1,9 @@
+let numero = parseInt(prompt("digite a tabuada de qual numero voce quer saber"))
+
+for (let i = numero; i <= numero + 2; i++){
+    console.log("tabuada do numero " + i)
+
+for (let j = 0; j <= 10; j++){
+    console.log(i + "x" + j + "=" +(i * j))
+}
+}
